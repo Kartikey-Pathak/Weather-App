b53e7f48dbf6afeee976254054f52774a5a2f64b
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-04-25 00:26:56 +0530

    Update Card.jsx Addded some ui effects

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,7 +8,7 @@ function Card(){
                 initial={{ y: -200, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ type: "spring", stiffness: 80 }}
-                className='bg-white h-96 w-80 absolute z-30 mt-[28rem] m-auto rounded-4xl overflow-y-auto'
+                className='bg-white/5 h-96 w-80 absolute z-30 mt-[28rem] m-auto rounded-4xl overflow-y-auto backdrop-brightness-80 backdrop-blur-lg shadow-[0_0_40px_#2323FF]'
               >
                 {/* Cross */}
                 <div className='absolute right-3 top-3'>
@@ -22,20 +22,20 @@ function Card(){
 
                 {/* Static Texts/Info */}
                 <div>
-                  <div className='h-12 w-20 rounded-4xl flex justify-center items-center m-5'><img className='rounded-4xl' src="/openweather.png" alt="" /></div>
-                  <h1 className='font-bold text-black m-3'>This weather app uses the</h1>
-                  <h1 className='font-bold text-black m-3'><a href="https://openweathermap.org/api" className=' font-medium text-blue-500 underline'>OpenWeather Api</a><br />to fetch real-time weather data.</h1>
+                  <div className='h-12 w-20 rounded-4xl flex justify-center items-center m-5'><img className='rounded-4xl' src="/src/assets/openweather.png" alt="" /></div>
+                  <h1 className='font-extrabold text-gray-500 m-3'>This weather app uses the</h1>
+                  <h1 className='font-bold text-gray-500 m-3'><a href="https://openweathermap.org/api" className=' font-medium text-blue-500 underline'>OpenWeather Api</a><br />to fetch real-time weather data.</h1>
                   <br />
                   <h1 className='font-bold text-black m-3'>- The Api Includes Some Special Features Like : -</h1>
                   <ul>
-                    <li className=' m-3 text-gray-500 font-bold'>- Access current weather data for any location</li>
-                    <li className=' m-3 text-gray-500 font-bold'>- We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations</li>
-                    <li className=' m-3 text-gray-500 font-bold'>- JSON, XML, and HTML formats</li>
-                    <li className=' m-3 text-gray-500 font-bold'>- Included in both free and paid subscriptions</li>
+                    <li className=' m-3 text-gray-300 font-bold'>- Access current weather data for any location</li>
+                    <li className=' m-3 text-gray-300 font-bold'>- We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations</li>
+                    <li className=' m-3 text-gray-300 font-bold'>- JSON, XML, and HTML formats</li>
+                    <li className=' m-3 text-gray-300 font-bold'>- Included in both free and paid subscriptions</li>
                   </ul>
-                  <h1 className='font-bold text-black m-3'>Other features</h1>
+                  <h1 className='font-bold text-gray-500 m-3'>Other features</h1>
                   <br />
-                  <h2 className='font-bold text-black m-3'>Geocoding API</h2>
+                  <h2 className='font-bold text-gray-500 m-3'>Geocoding API</h2>
                   <h1 className='font-bold text-gray-400 m-3'>Requesting API calls by geographical coordinates is the most accurate way to specify any location. If you need to convert city names and zip-codes to geo coordinates and the other way around automatically, please use our</h1><a className='m-3 font-medium text-blue-500 underline' href="https://openweathermap.org/api/geocoding-api">Geocoding API.</a>
                   <br />
                   <h1 className='font-bold text-black m-3'>Product concept</h1>
