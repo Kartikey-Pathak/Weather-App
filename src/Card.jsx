4758b586f3328b8c9cdf1b9d1d9dc9f046df26f2
Kartikey Pathak <kartikeypathak08@gmail.com>
2025-04-28 01:04:42 +0530

    Update Card.jsx added few ui text enhancements

--- src/Card.jsx
+++ src/Card.jsx
@@ -22,9 +22,9 @@ function Card(){
 
                 {/* Static Texts/Info */}
                 <div>
-                  <div className='h-12 w-20 rounded-4xl flex justify-center items-center m-5'><img className='rounded-4xl' src="/openweather.png" alt="" /></div>
-                  <h1 className='font-extrabold text-gray-500 m-3'>This weather app uses the</h1>
-                  <h1 className='font-bold text-gray-500 m-3'><a href="https://openweathermap.org/api" className=' font-medium text-blue-500 underline'>OpenWeather Api</a><br />to fetch real-time weather data.</h1>
+                  <div className='h-12 w-20 rounded-4xl flex justify-center items-center m-5'><img className='rounded-4xl' src="/src/assets/openweather.png" alt="" /></div>
+                  <h1 className='font-extrabold text-gray-800 m-3'>This weather app uses the</h1>
+                  <h1 className='font-bold text-gray-800 m-3'><a href="https://openweathermap.org/api" className=' font-medium text-blue-500 underline'>OpenWeather Api</a><br />to fetch real-time weather data.</h1>
                   <br />
                   <h1 className='font-bold text-black m-3'>- The Api Includes Some Special Features Like : -</h1>
                   <ul>
@@ -33,16 +33,16 @@ function Card(){
                     <li className=' m-3 text-gray-300 font-bold'>- JSON, XML, and HTML formats</li>
                     <li className=' m-3 text-gray-300 font-bold'>- Included in both free and paid subscriptions</li>
                   </ul>
-                  <h1 className='font-bold text-gray-500 m-3'>Other features</h1>
+                  <h1 className='font-bold text-gray-800 m-3'>Other features</h1>
                   <br />
-                  <h2 className='font-bold text-gray-500 m-3'>Geocoding API</h2>
-                  <h1 className='font-bold text-gray-400 m-3'>Requesting API calls by geographical coordinates is the most accurate way to specify any location. If you need to convert city names and zip-codes to geo coordinates and the other way around automatically, please use our</h1><a className='m-3 font-medium text-blue-500 underline' href="https://openweathermap.org/api/geocoding-api">Geocoding API.</a>
+                  <h2 className='font-bold text-gray-800 m-3'>Geocoding API</h2>
+                  <h1 className='font-bold text-gray-300 m-3'>Requesting API calls by geographical coordinates is the most accurate way to specify any location. If you need to convert city names and zip-codes to geo coordinates and the other way around automatically, please use our</h1><a className='m-3 font-medium text-blue-500 underline' href="https://openweathermap.org/api/geocoding-api">Geocoding API.</a>
                   <br />
                   <h1 className='font-bold text-black m-3'>Product concept</h1>
-                  <h2 className='font-bold text-gray-400 m-3'>Access current weather data for any location on Earth! We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations. Data is available in JSON, XML, or HTML format.</h2>
+                  <h2 className='font-bold text-gray-300 m-3'>Access current weather data for any location on Earth! We collect and process weather data from different sources such as global and local weather models, satellites, radars and a vast network of weather stations. Data is available in JSON, XML, or HTML format.</h2>
                   <br />
                   <br />
-                  <h1 className='font-bold text-gray-500 m-3'>~ Thanks For Using My react App, Developer Kartikey</h1>
+                  <h1 className='font-bold text-blue-900 m-3'>~ Thanks For Using My react App, Developer Kartikey</h1>
                 </div>
                 {/* Static Texts/Info */}
 
